a46d08354dd63ae5e5cd30944317b31381c31942
Michał Budzyński <budziq@gmail.com>
2014-02-02 21:45:15 +0100

    added popup nottifications to audio switching script

--- utils/switch_audio_out.sh
+++ utils/switch_audio_out.sh
@@ -4,10 +4,12 @@ if pacmd list-cards | grep active | grep hdmi 1>/dev/null; then
     echo "hdmi -> analog"
     #set analog out
     pacmd set-card-profile 0 output:analog-stereo+input:analog-stereo 1>/dev/null
+    notify-send -u critical -i audio-volume-medium-symbolic "Analog audio active"
 else
     echo "analog -> hdmi"
     #set hdmi out
     pacmd set-card-profile 0 output:hdmi-stereo+input:analog-stereo 1>/dev/null
+    notify-send -u critical -i audio-volume-medium-symbolic "HDMI audio active"
 fi
 
 
