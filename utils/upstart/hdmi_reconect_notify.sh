e9cf8a13eaaec5222fdddc3515ed603ef6d0c14c
Budziq <budziq@gmail.com>
2015-11-17 00:04:27 +0100

    added busy waiting for X11 display to actually appear TODO - xdpyinfo seams to not work in case of upstart environment and we just wait until loop runs out. Should fix it and refactor

--- utils/upstart/hdmi_reconect_notify.sh
+++ utils/upstart/hdmi_reconect_notify.sh
@@ -1,21 +1,30 @@
 #!/bin/bash
 
-START_DELAY=5
+START_DELAY=10
 #WATCHED_FILE="/sys/class/backlight/acpi_video0/brightness"
 WATCHED_FILE="/sys/class/drm/card0-HDMI-A-1/status"
 ACTION_STATE="connected"
-#sleep $START_DELAY
 
 
 OLD_STATE="dummy"
 
 while [ true ]; do
     STATE=$(cat $WATCHED_FILE)
-
+    export DISPLAY=:0.0
     if [ "$OLD_STATE" != "$STATE" ]; then
         if [ "$STATE" == "$ACTION_STATE" ]; then
+            echo "sleep 10 - reconnected display: $STATE"
+		  for T in $(seq 1 20); do
+		    if /usr/bin/xdpyinfo 1> /dev/null 2>&1; then
+		      echo "1yes-$T"; break
+		    fi
+		    echo "1no-$T"; sleep 0.5
+		  done
+
             echo "reconnected display: $STATE"
-	    DISPLAY=:0.0 xrandr --output HDMI-1 --auto
+	    su -c "DISPLAY=:0.0 /usr/bin/xrandr --output HDMI-1 --auto" odroid
+            echo "killing kodi: $STATE"
+	    pkill kodi
         fi
     fi
     echo "state: $STATE"
