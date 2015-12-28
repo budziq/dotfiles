95c19c6c56f75fd850240c1363215adc1172aa59
Michał Budzyński <budziq@gmail.com>
2015-12-28 12:05:57 +0100

    added slightly saner guake colorschemes

---
+++ setup/guake/set_guake_colors.sh
@@ -0,0 +1,1 @@
+./terminal_hybrid.sh
---
+++ setup/guake/terminal_smyck.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+gconftool-2 -s -t string /apps/guake/style/background/color '#2d2d2d2d2d2d'
+gconftool-2 -s -t string /apps/guake/style/font/color '#f8f8f8f8f8f8'
+gconftool-2 -s -t string /apps/guake/style/font/palette '#000000000000:#c7c756564646:#8e8eb3b33b3b:#d0d0b0b03c3c:#7272b3b3cccc:#c8c8a0a0d1d1:#212186869393:#b0b0b0b0b0b0:#5d5d5d5d5d5d:#e0e096969090:#cdcdeeee6969:#ffffe3e37777:#9c9cd9d9f0f0:#fbfbb1b1f9f9:#7777dfdfd8d8:#f7f7f7f7f7f7'
